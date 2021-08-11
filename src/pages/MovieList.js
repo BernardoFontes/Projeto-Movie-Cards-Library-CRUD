2f0bf67aabf6a33fd5bb6b7108d4d2821ac1c406
BernardoFontesNKWEB <bernardo.miranda@nkweb.com.br>
2021-08-11 15:47:24 -0300

    projeto mcl crud

---
+++ src/pages/MovieList.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
+import * as movieAPI from '../services/movieAPI';
+
+class MovieList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    movieAPI.getMovies().then((movies) => this.setState({ movies, loading: false }));
+  }
+
+  render() {
+    const { movies, loading } = this.state;
+
+    if (loading === true) {
+      return <Loading />;
+    }
+    return (
+      <div data-testid="movie-list">
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
+  }
+}
+export default MovieList;
